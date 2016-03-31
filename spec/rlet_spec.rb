5d113fd63d3332a95cb29ac5c9a6aca72cb50d59
Ho-Sheng Hsiao <talktohosh@gmail.com>
2016-03-31 15:44:53 -0700

    [SPEC] Fixed version test

--- spec/rlet_spec.rb
+++ spec/rlet_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
-describe Rlet do
+describe RLet do
   it 'has a version number' do
-    expect(Rlet::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+    expect(RLet::VERSION).not_to be nil
   end
 end
